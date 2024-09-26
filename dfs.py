46d46044917b456ce3d8caab5bde12d4e2925ae4
Yamunaasri <115707860+Yamunaasri@users.noreply.github.com>
2024-09-26 08:48:30 +0530

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -8,16 +8,19 @@ def dfs(graph,start,visited,path):
     path.append(start)
     visited[start]=True
     for neighbour in graph[start]:
-       #type ur code here
+        if visited[neighbour]==False:
+            dfs(graph,neighbour,visited,path)
+            visited[neighbour]=True
     return path
 graph=defaultdict(list)
 n,e=map(int,input().split())
 for i in range(e):
-   #type ur code here
+    u,v=map(str,input().split())
+    graph[u].append(v)
     graph[v].append(u)
 #print(graph)
 start='A'
 visited=defaultdict(bool)
- #type ur code here
+path=[]
 traversedpath=dfs(graph,start,visited,path)
 print(traversedpath)
